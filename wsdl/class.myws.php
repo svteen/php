52740896c89aa2f0406854bffd7c22106ad49a5a
svteen <svteen@users.noreply.github.com>
2014-10-18 10:59:51 +0800

    Create class.myws.php

---
+++ wsdl/class.myws.php
@@ -0,0 +1,11 @@
+<?php
+class Myws{   
+    public function show(){
+    	return 'webservice wsdl testting.';   
+    } 
+
+    public function getMax($one, $two){
+	return "aaabbbccc";
+    }  
+    
+}
